a3cff58f0d84e0693b60e2fb509466bcbe4b1a9f
Mirek Rup <miroslawrup7@gmail.com>
2022-10-19 21:25:49 +0200

    Budget 2

--- src/js/main.js
+++ src/js/main.js
@@ -21,33 +21,29 @@ if (currencyLoc_2) {
     })
 }
 
-// budget menu popup
+// BUDGET
+const budgetPageLoc = document.querySelector(".budget-page");
 
-const dotsLoc = document.querySelector(".title img");
-const menuPopupLoc = document.querySelector(".menu-popup");
+if (budgetPageLoc) {
 
-if (dotsLoc) {
+    // budget menu popup
+    const dotsLoc = document.querySelector(".title img");
+    const menuPopupLoc = document.querySelector(".menu-popup");
+
     dotsLoc.addEventListener("click", () => {
         menuPopupLoc.classList.toggle("active");
     });
-    
-}
 
-if (menuPopupLoc) {
     document.addEventListener("click", (e)=>{
         if (!e.target.closest(".menu-popup") && (!e.target.closest(".dots"))) {
             menuPopupLoc.classList.remove("active");
         }
     })
-}
-
-//  budget month activate
-
-const monthLoc = document.querySelectorAll(".months div");
-
-if (monthLoc) {
-    monthLoc.forEach((elem)=>{
     
+    //  budget month activate
+    const monthLoc = document.querySelectorAll(".months div");
+
+    monthLoc.forEach((elem)=>{
         elem.addEventListener("click", ()=>{
             monthLoc.forEach((elem)=>{
                 elem.classList.remove("active");
@@ -55,19 +51,116 @@ if (monthLoc) {
             elem.classList.add("active");
         })
     })
+
+    //  budget approving tooltip activate
+    const apprBtnLoc = document.querySelectorAll(".table-revenue .button")
+
+    apprBtnLoc.forEach((elem)=>{
+        elem.addEventListener("mouseover", (e)=>{
+            const apprBtnTooltipLoc = e.target.closest(".button").querySelector(".tooltip")
+            apprBtnTooltipLoc.classList.add("active");
+        });
+        elem.addEventListener("mouseout", (e)=>{
+            const apprBtnTooltipLoc = e.target.closest(".button").querySelector(".tooltip")
+            apprBtnTooltipLoc.classList.remove("active");
+        });
+    })
+
+    //  budget add new budget
+    const rightContentCoverLoc = document.querySelector(".right-content-cover");
+    
+    const addNewBudgetLoc = document.querySelector(".add-new-budget");
+    
+    const newBudgetPopupLoc = document.querySelector(".new-budget-popup")
+    const closeBudgetBtnLoc = document.querySelector(".new-budget-popup .close-btn")
+    const cancelBudgetBtnLoc = document.querySelector(".new-budget-popup .cancel-btn")
+
+    addNewBudgetLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.add("active");
+        menuPopupLoc.classList.remove("active");
+        newBudgetPopupLoc.classList.add("active");
+    })
+
+    closeBudgetBtnLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.remove("active");
+        menuPopupLoc.classList.add("active");
+        newBudgetPopupLoc.classList.remove("active");
+    })
+    
+    cancelBudgetBtnLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.remove("active");
+        menuPopupLoc.classList.add("active");
+        newBudgetPopupLoc.classList.remove("active");
+    })
+
+    const addNewRevenueLoc = document.querySelector(".add-new-revenue");
+    
+    const newRevenuePopupLoc = document.querySelector(".new-revenue-popup")
+    const closeRevenueBtnLoc = document.querySelector(".new-revenue-popup .close-btn")
+    const cancelRevenueBtnLoc = document.querySelector(".new-revenue-popup .cancel-btn")
+
+    addNewRevenueLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.add("active");
+        newRevenuePopupLoc.classList.add("active");
+    })
+
+    closeRevenueBtnLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.remove("active");
+        newRevenuePopupLoc.classList.remove("active");
+    })
+    
+    cancelRevenueBtnLoc.addEventListener("click", ()=>{
+        rightContentCoverLoc.classList.remove("active");
+        newRevenuePopupLoc.classList.remove("active");
+    })
+
+    //  budget edit category
+
+    const editCategoryBtnLoc = document.querySelector(".edit-cat-btn")
+    const pencilesLoc1 = document.querySelectorAll(".table-expenses .cell:nth-child(1) img")
+    const pencilesLoc2 = document.querySelectorAll(".table-expenses .cell:nth-child(2) img")
+    const binsLoc = document.querySelectorAll(".table-expenses .row .bin")
+    const yesNoButtonsLoc = document.querySelector(".table-expenses .yes-no-buttons")
+
+    const noButtonsLoc = document.querySelector(".table-expenses .yes-no-buttons .cancel-add")
+    const yesButtonsLoc = document.querySelector(".table-expenses .yes-no-buttons .btn.add")
+    
+    editCategoryBtnLoc.addEventListener("click", ()=>{
+        pencilesLoc1.forEach((elem)=>{
+            elem.classList.toggle("active");
+        })
+        pencilesLoc2.forEach((elem)=>{
+            elem.classList.toggle("active");
+        })
+        binsLoc.forEach((elem)=>{
+            elem.classList.toggle("active");
+        })
+        yesNoButtonsLoc.classList.toggle("active");
+    })
+
+    noButtonsLoc.addEventListener("click", ()=>{
+        pencilesLoc1.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        pencilesLoc2.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        binsLoc.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        yesNoButtonsLoc.classList.remove("active");
+    })
+
+    yesButtonsLoc.addEventListener("click", ()=>{
+        pencilesLoc1.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        pencilesLoc2.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        binsLoc.forEach((elem)=>{
+            elem.classList.remove("active");
+        })
+        yesNoButtonsLoc.classList.remove("active");
+    })
 }
-
-//  budget approving tooltip activate
-
-const apprBtnLoc = document.querySelectorAll(".table-revenue .button")
-
-apprBtnLoc.forEach((elem)=>{
-    elem.addEventListener("mouseover", (e)=>{
-        const apprBtnTooltipLoc = e.target.closest(".button").querySelector(".tooltip")
-        apprBtnTooltipLoc.classList.add("active");
-    });
-    elem.addEventListener("mouseout", (e)=>{
-        const apprBtnTooltipLoc = e.target.closest(".button").querySelector(".tooltip")
-        apprBtnTooltipLoc.classList.remove("active");
-    });
-})
